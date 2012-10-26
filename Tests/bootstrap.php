164f96b747464a900735215d61a5bc338a39acf3
thewilkybarkid <chriswilkinson84@gmail.com>
2012-10-26 13:09:56 +0100

    Add license

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the MisdRavenBundle for Symfony2.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $loader = @include __DIR__ . '/../vendor/autoload.php';
 if (!$loader) {
     die(<<<'EOT'
