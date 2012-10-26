79b09efa34c716bb7a9519dc4714b6ec031d0b9d
thewilkybarkid <chriswilkinson84@gmail.com>
2012-10-26 15:51:30 +0100

    Create test bundle with secured and unsecured routes

---
+++ Tests/Functional/src/TestBundle/Controller/TestController.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the MisdRavenBundle for Symfony2.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\RavenBundle\Tests\Functional\src\TestBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+/**
+ * TestController.
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+class TestController extends ContainerAware
+{
+    public function unsecuredAction()
+    {
+        return new Response('This is unsecured.');
+    }
+
+    public function securedAction()
+    {
+        return new Response(sprintf(
+            'This is secured. You are %s.',
+            $this->container->get('security.context')->getToken()->getUsername()
+        ));
+    }
+}
