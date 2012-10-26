4fdb79c7c0b0fded298d2ffbd2ea6647b4eb14c9
thewilkybarkid <chriswilkinson84@gmail.com>
2012-10-26 15:49:33 +0100

    Replicate the Raven service for functional testing

---
+++ Tests/Functional/src/RavenBundle/RavenBundle.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the MisdRavenBundle for Symfony2.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\RavenBundle\Tests\Functional\src\RavenBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * RavenBundle.
+ *
+ * This replicates the functionality of the
+ * {@link http://raven.cam.ac.uk/project/test-demo/ test Raven service} for
+ * functional testing.
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+class RavenBundle extends Bundle
+{
+}
